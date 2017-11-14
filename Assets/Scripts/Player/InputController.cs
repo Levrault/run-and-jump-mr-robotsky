2296e166c4401a8328d8824be7db2ca8a5c543f6
Luc-Frédéric Langis <luc-frederic.langis@ekino.com>
2017-11-14 13:56:52 +0100

    feat(player): implement a basic system to let the player walk

---
+++ Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+	public PlayerMovement playerMovement;
+
+	void Awake() {
+		playerMovement = (PlayerMovement) GetComponent(typeof(PlayerMovement));
+	}
+
+	void Update() {
+		SetPlayerDirectionalInput (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		ControlPlayerMovement();
+	}
+
+	/// <summary>
+	/// Sets the player mouvement directional input.
+	/// </summary>
+	/// <returns>The player directional input.</returns>
+	/// <param name="horizontal">Horizontal.</param>
+	/// <param name="vertical">Vertical.</param>
+	private void SetPlayerDirectionalInput (float horizontal, float vertical)	{
+		playerMovement.SetDirectionalInput (new Vector2 (horizontal, vertical));
+	}
+
+	/// <summary>
+	/// Control the player's movement with the axis input
+	/// </summary>
+	private void ControlPlayerMovement() {
+		// moving
+		if (Input.GetAxisRaw("Horizontal") != 0) {
+
+			// running or walking
+			if (Input.GetButtonDown("Run")) {
+				playerMovement.Run();
+			} else {
+				playerMovement.Walk();
+			}
+		} else {
+			playerMovement.Idle();
+		}
+	}
+}
