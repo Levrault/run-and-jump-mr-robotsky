c092fe018f5f88b1af73bcf412b4b105f6a63724
Luc-Frédéric Langis <luc-frederic.langis@ekino.com>
2017-11-16 16:58:15 +0100

    feat(editor): add new ominisharp indentation file

--- Assets/Scripts/Player/InputController.cs
+++ Assets/Scripts/Player/InputController.cs
@@ -3,42 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputController : MonoBehaviour {
-	public PlayerMovement playerMovement;
+  public PlayerMovement playerMovement;
 
-	void Awake() {
-		playerMovement = (PlayerMovement) GetComponent(typeof(PlayerMovement));
-	}
+  void Awake() {
+    playerMovement = (PlayerMovement) GetComponent(typeof(PlayerMovement));
+  }
 
-	void Update() {
-		SetPlayerDirectionalInput (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
-		ControlPlayerMovement();
-	}
+  void Update() {
+    SetPlayerDirectionalInput(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    ControlPlayerMovement();
+  }
 
-	/// <summary>
-	/// Sets the player mouvement directional input.
-	/// </summary>
-	/// <returns>The player directional input.</returns>
-	/// <param name="horizontal">Horizontal.</param>
-	/// <param name="vertical">Vertical.</param>
-	private void SetPlayerDirectionalInput (float horizontal, float vertical)	{
-		playerMovement.SetDirectionalInput (new Vector2 (horizontal, vertical));
-	}
+  /// <summary>
+  /// Sets the player mouvement directional input.
+  /// </summary>
+  /// <returns>The player directional input.</returns>
+  /// <param name="horizontal">Horizontal.</param>
+  /// <param name="vertical">Vertical.</param>
+  private void SetPlayerDirectionalInput(float horizontal, float vertical) {
+    playerMovement.SetDirectionalInput(new Vector2(horizontal, vertical));
+  }
 
-	/// <summary>
-	/// Control the player's movement with the axis input
-	/// </summary>
-	private void ControlPlayerMovement() {
-		// moving
-		if (Input.GetAxisRaw("Horizontal") != 0) {
+  /// <summary>
+  /// Control the player's movement with the axis input
+  /// </summary>
+  private void ControlPlayerMovement() {
+    // moving
+    if (Input.GetAxisRaw("Horizontal") != 0) {
 
-			// running or walking
-			if (Input.GetButtonDown("Run")) {
-				playerMovement.Run();
-			} else {
-				playerMovement.Walk();
-			}
-		} else {
-			playerMovement.Idle();
-		}
-	}
+      // running or walking
+      if (Input.GetButtonDown("Run")) {
+        playerMovement.Run();
+      } else {
+        playerMovement.Walk();
+      }
+    } else {
+      playerMovement.Idle();
+    }
+  }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,79 +6,79 @@ using UnityEngine;
 /// Manage all the player mouvement (walk, run, jump, wall jump etc.)
 /// </summary>
 public class PlayerMovement : MonoBehaviour {
-	// Player's component
-	private Rigidbody2D rigidBody2D;
-	private Animator animator;
-	private SpriteRenderer spriteRenderer;
+  // Player's component
+  private Rigidbody2D rigidBody2D;
+  private Animator animator;
+  private SpriteRenderer spriteRenderer;
 
-	// player's params
-	public float walkingSpeed = 4f;
-	public float runningSpeed = 9.0f;
+  // player's params
+  public float walkingSpeed = 4f;
+  public float runningSpeed = 9.0f;
 
-	// private
-	private Vector3 velocity;
-	private Vector2 directionalInput;
-	private bool isFlippedRight = true;
-	private float speed = 0.0f;
+  // private
+  private Vector3 velocity;
+  private Vector2 directionalInput;
+  private bool isFlippedRight = true;
+  private float speed = 0.0f;
 
-	void Awake() {
-		rigidBody2D = (Rigidbody2D) GetComponent(typeof(Rigidbody2D));
-		animator = (Animator) GetComponent(typeof(Animator));
-		spriteRenderer = (SpriteRenderer) GetComponent(typeof(SpriteRenderer));
-	}
+  void Awake() {
+    rigidBody2D = (Rigidbody2D) GetComponent(typeof(Rigidbody2D));
+    animator = (Animator) GetComponent(typeof(Animator));
+    spriteRenderer = (SpriteRenderer) GetComponent(typeof(SpriteRenderer));
+  }
 
-	void Update() {
-		CalculVelocity(speed);
-		SetSpriteRendererDirection();
-	}
+  void Update() {
+    CalculVelocity(speed);
+    SetSpriteRendererDirection();
+  }
 
-	public void Idle() {
-		animator.SetBool("isIdle", true);
-		animator.SetBool("isWalking", false);
-		animator.SetBool("isRunning", false);
-	}
+  public void Idle() {
+    animator.SetBool("isIdle", true);
+    animator.SetBool("isWalking", false);
+    animator.SetBool("isRunning", false);
+  }
 
-	public void Walk() {
-		speed = walkingSpeed;
-		animator.SetBool("isIdle", false);
-		animator.SetBool("isWalking", true);
-	}
+  public void Walk() {
+    speed = walkingSpeed;
+    animator.SetBool("isIdle", false);
+    animator.SetBool("isWalking", true);
+  }
 
-	public void Run() {
-		speed = runningSpeed;
-		animator.SetBool("isIdle", false);
-		animator.SetBool("isRunning", true);
-	}
+  public void Run() {
+    speed = runningSpeed;
+    animator.SetBool("isIdle", false);
+    animator.SetBool("isRunning", true);
+  }
 
-	/// <summary>
-	/// Move the player left/right
-	/// </summary>
-	/// <param name="velocitySpeed"></param>
-	public void CalculVelocity(float velocitySpeed) {
-		Vector2 velocity = new Vector2(directionalInput.x * velocitySpeed, rigidBody2D.velocity.y);
-		rigidBody2D.velocity = velocity;
-	}
+  /// <summary>
+  /// Move the player left/right
+  /// </summary>
+  /// <param name="velocitySpeed"></param>
+  public void CalculVelocity(float velocitySpeed) {
+    Vector2 velocity = new Vector2(directionalInput.x * velocitySpeed, rigidBody2D.velocity.y);
+    rigidBody2D.velocity = velocity;
+  }
 
-	/// <summary>
-	/// Set spriteRenderer flipping direction when
-	/// directionInput change
-	/// </summary>
-	public void SetSpriteRendererDirection() {
-		if (directionalInput.x < 0 && isFlippedRight) {
-			spriteRenderer.flipX = true;
-			isFlippedRight = false;
-		} else if (!isFlippedRight && directionalInput.x > 0) {
-			spriteRenderer.flipX = false;
-			isFlippedRight = true;
-		}
-	}
+  /// <summary>
+  /// Set spriteRenderer flipping direction when
+  /// directionInput change
+  /// </summary>
+  public void SetSpriteRendererDirection() {
+    if (directionalInput.x < 0 && isFlippedRight) {
+      spriteRenderer.flipX = true;
+      isFlippedRight = false;
+    } else if (!isFlippedRight && directionalInput.x > 0) {
+      spriteRenderer.flipX = false;
+      isFlippedRight = true;
+    }
+  }
 
-	/// <summary>
-	/// Sets the directional input.
-	/// </summary>
-	/// <returns>The directional input.</returns>
-	/// <param name="Input">Input.</param>
-	public void SetDirectionalInput(Vector2 Input) {
-		directionalInput = Input;
-	}
+  /// <summary>
+  /// Sets the directional input.
+  /// </summary>
+  /// <returns>The directional input.</returns>
+  /// <param name="Input">Input.</param>
+  public void SetDirectionalInput(Vector2 Input) {
+    directionalInput = Input;
+  }
 }
