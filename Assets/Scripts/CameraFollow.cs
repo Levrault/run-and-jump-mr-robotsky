55f60c93e542ead22a8fd53f1f631ff8e9e165e2
Luc-Frédéric Langis <luc-frederic.langis@ekino.com>
2017-11-28 09:27:22 +0100

    feat(CameraFollow): add camera behavior for debug and in game mode

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Like in old super mario bros game level, the camera
+/// will always goes to the right. We can also activate a
+/// test mode where the camera follow the player 
+/// </summary>
+public class CameraFollow : MonoBehaviour {
+
+  // Distance between player and camera in horizontal direction
+  public float xOffset = 0f;
+
+  // Distance between player and camera in vertical direction
+  public float yOffset = 0f;
+
+  // follow the player if debugmode is on
+  public bool debugMode = false;
+
+  // how fast the camera goes to the right
+  public float speed = 2f;
+
+  // Reference to the player's transform.
+  public Transform player;
+
+  void Update() {
+    if (!debugMode) {
+      InGameMode();
+    }
+  }
+
+  void LateUpdate() {
+    if (debugMode) {
+      DebugMode();
+    }
+  }
+
+  /// <summary>
+  /// Follow player
+  /// </summary>
+  private void DebugMode() {
+    float positionX = player.transform.position.x + xOffset;
+    float positionY = transform.position.y + yOffset;
+    transform.position = new Vector3(positionX, positionY, -10);
+  }
+
+  /// <summary>
+  /// Always goes to the right
+  /// </summary>
+  private void InGameMode() {
+    transform.Translate(Vector3.right * Time.deltaTime * speed, Camera.main.transform);
+  }
+}
