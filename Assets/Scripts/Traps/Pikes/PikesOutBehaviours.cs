2d65295e97dda9dd7638981712814d0df176deb4
Luc-Frédéric Langis <lucf.langis@gmail.com>
2018-01-18 15:52:33 +0100

    feat(corgi): hide player sprite on death

--- Assets/Scripts/Traps/Pikes/PikesOutBehaviours.cs
+++ Assets/Scripts/Traps/Pikes/PikesOutBehaviours.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PikesOutSmb : StateMachineBehaviour {
+public class PikesOutBehaviours : StateMachineBehaviour {
+
   // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
   override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
     animator.gameObject.GetComponent<Pikes>().ActiveDamageCollider();
