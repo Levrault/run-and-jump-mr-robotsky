832576cf17168bb80fe6915bd00fbc2f3a36a2a3
Luc-Frédéric Langis <lucf.langis@gmail.com>
2018-01-19 13:31:46 +0100

    feat(corgi): upgrade collectable to be compatible with corgi engine

--- Assets/Scripts/Collectable/Collectable.cs
+++ Assets/Scripts/Collectable/Collectable.cs
@@ -1,27 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
 
-/// <summary>
-/// Collectable, increase health and player score
-/// </summary>
-public class Collectable : MonoBehaviour {
-
-  public int amount = 100;
-  private Animator animator;
-
-  void Start() {
-		animator = GetComponent<Animator>();
-  }
-
+namespace MoreMountains.CorgiEngine {
   /// <summary>
-  /// Sent when another object enters a trigger collider attached to this
-  /// object (2D physics only).
+  /// Coin manager
   /// </summary>
-  /// <param name="other">The other Collider2D involved in this collision.</param>
-  void OnTriggerEnter2D(Collider2D other) {
-    if (other.tag == "Player") {
-			animator.SetTrigger("isCollected");
+  [AddComponentMenu("Corgi Engine/Items/Collectable")]
+  public class Collectable : PickableItem {
+    /// The amount of points to add when collected
+    public int PointsToAdd = 1000;
+    private Animator _animator;
+
+    protected override void Start() {
+      base.Start();
+      _animator = gameObject.GetComponent<Animator>();
+		}
+
+    /// <summary>
+    /// Triggered when something collides with the coin
+    /// </summary>
+    /// <param name="collider">Other.</param>
+    protected override void Pick() {
+      // we send a new points event for the GameManager to catch (and other classes that may listen to it too)
+      MMEventManager.TriggerEvent(new CorgiEnginePointsEvent(PointsMethods.Add, PointsToAdd));
+      _animator.SetTrigger("isCollected");
     }
   }
 }
